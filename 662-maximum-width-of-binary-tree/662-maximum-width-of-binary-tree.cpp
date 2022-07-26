0aa3f6998dc8a582394f16a826ca8ae1ddff4a7f
Ankur Kumar <61647822+ankursharma6084@users.noreply.github.com>
2022-07-27 00:00:52 +0530

    Time: 10 ms (80.98%), Space: 17.4 MB (51.60%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,74 +12,45 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root) return 0;
-        queue<pair<TreeNode*,unsigned long long int>>q;
-        q.push({root,1});TreeNode* temp;
-        int size;int res=0;int d;
-        while(!q.empty()){
-            size=q.size();
-            if (size == 1){
-                q.push({q.front().first,1});
+        if(root == NULL)
+            return 0;
+        
+        int res = 1;
+        queue<pair<TreeNode*, int>> q;
+        
+        // I am using intialising list
+        q.push({root, 0});      // also can use make_pair
+        
+        while(!q.empty())
+        {
+            int cnt = q.size();
+            // start is the index of root node for first level
+            int start = q.front().second;
+            int end = q.back().second;
+            
+            res = max(res,end-start + 1);
+            
+            for(int i = 0; i <cnt; ++i)
+            {
+                pair<TreeNode*, int> p = q.front();
+                // we will use it while inserting it children
+                // left child will be 2 * idx + 1;
+                // right chils will be 2 * idx + 2;
+                int idx = p.second - start;
+                
                 q.pop();
-            }
-            d= q.back().second-q.front().second+1;
-            res=max(res,d);
-            while(size--){
-                if(q.front().first->left) q.push({q.front().first->left,(q.front().second*2)});
-                if(q.front().first->right) q.push({q.front().first->right,((q.front().second*2)+1)});
-                q.pop();
+                
+                // if  left child exist
+                if(p.first->left != NULL)
+                    q.push({p.first->left, (long long)2 * idx + 1});
+                
+                // if right child exist
+                if(p.first->right != NULL)
+                    q.push({p.first->right, (long long) 2 * idx + 2});
             }
         }
+        
         return res;
+        
     }
 };
-
-
-
-// class Solution {
-// public:
-//     int widthOfBinaryTree(TreeNode* root) {
-        
-//         int ans = 0 ;
-        
-//         queue< pair< TreeNode* , int> > q ;
-//         q.push({ root , 0 } );
-        
-//         while(q.size() > 0)
-//         {
-//             int sz = q.size();
-            
-//             if(sz == 1)
-//             {
-//                 q.push( { q.front().first , 0 } );
-//                 q.pop();
-//             }
-            
-//             int f = q.front().second ;
-//             int curw = 0 ;
-            
-//             while(sz--)
-//             {
-//                 TreeNode* top = q.front().first;
-//                 int id = q.front().second - f ;
-//                 curw = max(curw , id) ;
-                
-//                 q.pop();
-                
-//                 if(top->left)
-//                 {
-//                     q.push({ top->left , 2*id } );
-//                 }
-                
-//                 if(top->right)
-//                 {
-//                     q.push( { top->right , 2*id+1 } );
-//                 }
-//             }
-            
-//             ans = max(ans , curw);
-//         }
-        
-//         return ans+1 ;
-//     }
-// };
